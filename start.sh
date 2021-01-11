06e79394462bf50a6136bbf3a5bf552a9690a0d6
Stefan Oehmcke <stefan.oehmcke@uni-oldenburg.de>
2021-01-11 18:01:41 +0100

    reduce bs

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 data=${1:-./data/train_test_whole_class_1km}
-python  train.py  --model pointnet2msg  --log-dir pointnet2_sem_seg_testll --batch-size 16  --data-path $data --blocks-per-epoch 2048
+python  train.py  --model pointnet2msg  --log-dir pointnet2_sem_seg_testll --batch-size 8  --data-path $data --blocks-per-epoch 2048
